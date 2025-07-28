cb3ddcf93ab1a2dd662df028b67bb1c0c77bf607
saif.alshaiban@sol.sa <saif.alshaiban@sol.sa>
2025-07-28 23:10:09 +0300

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+   
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.my-app-dark', // ← manual mode
+          cssLayer: false,
+          ripple: true
+        }
+      }
+    })
+
+  ]
 };
